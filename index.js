b45095cedbfe233ddf685e4b808b6bc5eb6b0d42
ouznoreyni <ousmanediopp268@gmail.com>
2020-05-01 11:26:21 +0000

    add replaceTemplate module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http')
 const url = require('url')
 const fs = require('fs')
 
+const replaceTemplate = require('./modules/replaceTemplate')
 
 
 const templateOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
@@ -10,23 +11,6 @@ const templateProduct = fs.readFileSync(`${__dirname}/templates/product.html`, '
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
-const replaceTemplate = (temp, product)=>{
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-
-
-  return output
-}
 
 
 
